e8f322c221b8a680564bd505579b0cfad1eae4e1
Kristopher <kristopher.williams2@gmail.com>
2020-04-10 00:25:15 -0500

    finished solutions

---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styles: [`
+    .hidden {
+      visibility: hidden;
+    }
+    .greater {
+      color: white;
+    }
+  `]
+})
+export class AppComponent {
+  visible = true;
+  clicked = false
+  countArray = []
+  count = 0
+
+  changeVisibility() {
+    this.visible = !this.visible;
+    this.countArray.push(this.countArray.length + 1)
+  }
+
+  getColor(){
+    return this.count >= 5 ? "blue" : null
+  }
+
+
+}
